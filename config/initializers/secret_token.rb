1d08f8bac4c03345bf0415264ab2d30363234eb8
Cuong Bui <bpcuong2013@gmail.com>
2013-10-15 13:46:48 +0700

    Initialize source

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YetiFinderRails::Application.config.secret_token = '507b597a898ea1a5c3697806c8996f785ece8e14446e836f8e90d3d2bce1372a115b462a7b16c7e2dec6bb4d1d52eb6f561b9204c59bed61817bcf8a67828a3d'
