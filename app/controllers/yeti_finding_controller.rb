05d287164cd65f688729dc5ccda9cf7a51061735
Cuong Bui <bpcuong2013@gmail.com>
2013-10-18 15:52:12 +0700

    Add some methods to controller

--- app/controllers/yeti_finding_controller.rb
+++ app/controllers/yeti_finding_controller.rb
@@ -74,7 +74,75 @@ class YetiFindingController < ApplicationController
     end
   end
   
+  # find all of yetis in range
   def findYetis
+    longitude = convertFloat(params[:longitude])
+    latitude = convertFloat(params[:latitude])
+    user_id = params[:user_id]
     
+    user = User.find_by_id(user_id)
+    found_yeti_ids = user.finds.map { |m| m[:yeti_id] }
+    
+    available_yetis = Yeti.where("city_id = :city_id AND id NOT IN :yeti_ids", { city_id: user.city_id, yeti_ids: found_yeti_ids })
+    in_range_yetis = []
+    
+    haversineHelper = HaversineHelper.new
+    available_yetis.each do |yeti|
+      if haversineHelper.distanceInMile(latitude, longitude, convertFloat(yeti.lat), convertFloat(yeti.long)) <= 5000 # compare values in miles
+        in_range_yetis.push yeti
+      end
+    end
+    
+    respond_to do |format|
+      format.json {
+        render :json => { :success => true, :data => in_range_yetis }
+      }
+    end
+  rescue Exception => ex
+    logger.fatal "Exception in findYetis action: " + ex.message
+    respond_to do |format|
+      format.json {
+        render :json => { :success => false, :msg => ex.message }
+      }
+    end
+  end
+  
+  # Save yeti found
+  def saveYetiFound
+    user_id = params[:user_id]
+    yeti_id = params[:yeti_id]
+    
+    yetiFound = Find.where("user_id = :user_id AND yeti_id = :yeti_id", { user_id: user_id, yeti_id: yeti_id })
+    if yetiFound == nil
+      yetiFound = Find.new
+      yetiFound.user_id = user_id
+      yetiFound.yeti_id = yeti_id
+      
+      yetiFound.save
+    end
+    
+    respond_to do |format|
+      format.json {
+        render :json => { :success => true, :data => "Successfully saved" }
+      }
+    end
+  rescue Exception => ex
+    logger.fatal "Exception in saveYetiFound action: " + ex.message
+    respond_to do |format|
+      format.json {
+        render :json => { :success => false, :msg => ex.message }
+      }
+    end
+  end
+  
+  def getScoreboard
+    user_id = params[:user_id]
+  rescue Exception => ex
+    logger.fatal "Exception in getScoreboard action: " + ex.message
+    respond_to do |format|
+      format.json {
+        render :json => { :success => false, :msg => ex.message }
+      }
+    end
   end
 end
---
+++ lib/haversine_helper.rb
@@ -0,0 +1,40 @@
+# Source of this class is from http://www.esawdust.com/blog/gps/files/HaversineFormulaInRuby.html 
+
+class HaversineHelper
+  RADIAN_PER_DEGREE = Math::PI / 180.0
+  RADIUS_MILE = 3956    # radius of the great circle in miles
+  RADIUS_KM = 6371      # radius in kilometers ... some algorithms use 6367
+  RADIUS_FEET = RADIUS_MILE * 5282 # radius in feet
+  RADIUS_METER = RADIUS_KM * 1000 # radius in meters
+  
+  def distanceInKm(lat1, lng1, lat2, lng2)
+    return RADIUS_KM * getCoefficient(lat1, lng1, lat2, lng2)
+  end
+  
+  def distanceInMile(lat1, lng1, lat2, lng2)
+    return RADIUS_MILE * getCoefficient(lat1, lng1, lat2, lng2)
+  end
+  
+  def distanceInFeet(lat1, lng1, lat2, lng2)
+    return RADIUS_FEET * getCoefficient(lat1, lng1, lat2, lng2)
+  end
+  
+  def distanceInMeter(lat1, lng1, lat2, lng2)
+    return RADIUS_METER * getCoefficient(lat1, lng1, lat2, lng2)
+  end
+  
+  private
+  def getCoefficient(lat1, lng1, lat2, lng2)
+    dlng = lng1 - lng2
+    
+    lat1_rad = lat1 * RADIAN_PER_DEGREE
+    lat2_rad = lat2 * RADIAN_PER_DEGREE
+    dlng_rad = dlng * RADIAN_PER_DEGREE
+    dlat_rad = lat1_rad - lat2_rad
+    
+    a = power(Math::sin(dlat_rad/2), 2) + Math::cos(lat1_rad) * Math::cos(lat2_rad) * power(Math::sin(dlng_rad/2), 2)
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+    
+    return c
+  end
+end
