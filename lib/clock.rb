adcd9f395bce90f93440570e268290de11ded2ea
Cuong Bui <bpcuong2013@gmail.com>
2013-10-19 09:10:59 +0700

    Finish debug schedule work

--- lib/clock.rb
+++ lib/clock.rb
@@ -7,6 +7,22 @@ require './app/workers/schedule_worker'
 
 include Clockwork
 
+#every(2.minutes, 'generateRandomYetisDaily.job') {
+#  puts "Run 2 minutely job at " + Time.now.to_s
+#  cities = City.all
+#  FoursquareHelper.new.selectSearchTerm()
+#  
+#  puts "Total daily schedule = " + cities.size.to_s
+#  puts "New search term = " + FoursquareHelper.SELECTED_SEARCH_TERM
+#  
+#  if (cities.size > 0)
+#    cities.each do |city|
+#      puts city.name
+#      ScheduleWorker.perform_async(city.id, city.name)
+#    end
+#  end
+#}
+
 every(1.day, 'generateRandomYetisDaily.job', :at => '23:59') {
   puts "Run daily job at " + Time.now.to_s
   cities = City.all
