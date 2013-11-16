4a64e590f14e526bc098cf21315038b1cda79f58
Cuong Bui <bpcuong2013@gmail.com>
2013-11-16 11:36:13 +0700

    Disable scheduler

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ ruby '1.9.3'
 source 'https://rubygems.org'
 gem 'rails', '3.2.12'
 gem 'json'
-gem 'sqlite3'
 
 gem 'mechanize'
 gem 'rest-client'
@@ -10,6 +9,10 @@ gem 'clockwork', '0.2.0'
 gem 'sidekiq'
 gem 'crack' # for parsing xml
 gem 'log4r', '1.1.9'
+
+group :development do
+  gem 'sqlite3'
+end
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- lib/clock.rb
+++ lib/clock.rb
@@ -1,11 +1,11 @@
-require 'rubygems'
-require 'clockwork'
-require 'date'
-require './config/boot'
-require './config/environment'
-require './app/workers/schedule_worker'
+#require 'rubygems'
+#require 'clockwork'
+#require 'date'
+#require './config/boot'
+#require './config/environment'
+#require './app/workers/schedule_worker'
 
-include Clockwork
+#include Clockwork
 
 #every(2.minutes, 'generateRandomYetisDaily.job') {
 #  puts "Run 2 minutely job at " + Time.now.to_s
@@ -23,17 +23,17 @@ include Clockwork
 #  end
 #}
 
-every(1.day, 'generateRandomYetisDaily.job', :at => '23:59') {
-  puts "Run daily job at " + Time.now.to_s
-  cities = City.all
-  FoursquareHelper.new.selectSearchTerm()
-  
-  puts "Total daily schedule = " + cities.size.to_s
-  puts "New search term = " + FoursquareHelper.SELECTED_SEARCH_TERM
-  
-  if (cities.size > 0)
-    cities.each do |city|
-      ScheduleWorker.perform_async(city.id, city.name)
-    end
-  end
-}
+#every(1.day, 'generateRandomYetisDaily.job', :at => '23:59') {
+#  puts "Run daily job at " + Time.now.to_s
+#  cities = City.all
+#  FoursquareHelper.new.selectSearchTerm()
+#  
+#  puts "Total daily schedule = " + cities.size.to_s
+#  puts "New search term = " + FoursquareHelper.SELECTED_SEARCH_TERM
+#  
+#  if (cities.size > 0)
+#    cities.each do |city|
+#      ScheduleWorker.perform_async(city.id, city.name)
+#    end
+#  end
+#}
